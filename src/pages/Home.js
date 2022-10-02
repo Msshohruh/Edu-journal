238fe509b9919a0adbee3a89385595109817250d
Msshohruh <msshohruh@gmail.com>
2022-10-02 13:51:01 +0500

    add imagse

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,62 @@ function Home() {
   return (
     <>
       <h2 className="content-title">Home</h2>
+      <div className="sertificates sertificates-right">
+        <img
+          className="sertificates-img"
+          src="./images/impact-factor.png"
+          alt="impact-factor"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/iso.png"
+          alt="iso"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/issn.jpg"
+          alt="issn"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/SJI.png"
+          alt="SJI"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/doi.jpg"
+          alt="doi"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/zenodo.png"
+          alt="zenodo"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/Citefactor.png"
+          alt="Citefactor"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/openaire.png"
+          alt="Citefactor"
+          width="250"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/oa.png"
+          alt="Citefactor"
+          width="250"
+        />
+      </div>
       <ul className="home-book-list">
         <li className="home-book-list-item">
           <img className="item-img" src="./images/img1.jpg" alt="" />
@@ -40,6 +96,62 @@ function Home() {
           </div>
         </li>
       </ul>
+      <div className="sertificates sertificates-bottom">
+        <img
+          className="sertificates-img"
+          src="./images/impact-factor.png"
+          alt="impact-factor"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/iso.png"
+          alt="iso"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/issn.jpg"
+          alt="issn"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/SJI.png"
+          alt="SJI"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/doi.jpg"
+          alt="doi"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/zenodo.png"
+          alt="zenodo"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/Citefactor.png"
+          alt="Citefactor"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/openaire.png"
+          alt="Citefactor"
+          width="150"
+        />
+        <img
+          className="sertificates-img"
+          src="./images/oa.png"
+          alt="Citefactor"
+          width="150"
+        />
+      </div>
     </>
   );
 }
